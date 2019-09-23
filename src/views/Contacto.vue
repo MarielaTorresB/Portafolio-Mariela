f46582b1853f47bdec1811153e7ad236907247f4
Mariela Torres <martz_maru@hotmail.com>
2019-09-23 14:50:27 -0500

    creando las rutas y las vistas

---
+++ src/views/Contacto.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="contacto">
+    <h1>Mis datos de contacto</h1>
+  </div>
+</template>
---
+++ src/views/Proyectos.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="proyectos">
+    <h1>Aquí estarán mis proyectos</h1>
+    <img src="https://memecreator.org/static/images/memes/5106228.jpg">
+  </div>
+</template>
