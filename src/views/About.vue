80844210bf04ed1c58b39700b2511f3ce59667be
Mariela Torres <martz_maru@hotmail.com>
2019-09-25 15:22:25 -0500

    Agregando imagen para el about Me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
     <h1>Aquí va el about me</h1>
-    <img src="https://getstickerpack.com/storage/uploads/sticker-pack/el-pack-de-bellota-ppg/sticker_1.webp?2a58a11796bc7760f68bc9afc16b4080"><img>
+        <img src="https://lh3.googleusercontent.com/sJw9YT9nujWrb7Ef9BBO468Lddc4wMWWDFRoFP6BJvAJBghh0Z5Z5TPMOdfA3YCqO-X213S87FpAb1xLkRSy-DhQLsJPWUTKXEJHbnYG3qSON6vlDgOm-FPRLtlpoSs8zdHN4cpECGtP0XNh-tnvuOIFYvRkJw9213iTEeMK5HBcVjfGY7OBmUbDtu06VSBKrn9rIUcy2V8rK-zPY13-zCYqtCun9EoBxxG32KFhXggikMQNunuSyj9UdkO6InHNC22n53lKZtwSWCJY6zUiu-XRdd4tPPI80ReDJ_KtCfM-n0htA7KJEvu9z2WE9SWvYGvcLED7RYwJoVRfuQkoYPzAGEnU7LyVUk4E64iKEkZ9SzIL3aDK3b8YcVkxcYiPhgSSUUuYFKNlI1n9p5ulkCFQIMO8BGWGDi_cwywFQFS7sylDr934eC5iTgznkwYw466H4txQuOM9VaxzKKY7KAmTjp6dVC1wXwJ1FQjz8YDWVW1zTfGYktA6EP8EZup9y2RYsUpoYQHuxShp2QgU7QsPN4jBBhFEewjps9q52t5kcKWn5xi1Iq86eqs6tHhBECmawUY8t3lHBfkoUZkyyvLLDuL6z3jEhVPfKfnIHIImYR7878yyCCrQZLn8zE5_yOSU6hDncoo_zgSEuURCus1XaXzIkQXGsqtn9iEE95cI-d8VoZqN8TuaMvJb-WzFO-4DxNKtaimx97mHyztDXc9zkjUs3ibns1zD3WCSAH_fWh4v=s96-no" width=30% height=30%></img>
+    <!-- <img src="https://getstickerpack.com/storage/uploads/sticker-pack/el-pack-de-bellota-ppg/sticker_1.webp?2a58a11796bc7760f68bc9afc16b4080"><img> -->
     <Yo-comp></Yo-comp>
   </div>
 </template>
