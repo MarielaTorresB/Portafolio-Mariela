b63c0ea165ffe041aca4b1ac86a1fe158faa98f2
Mariela Torres <martz_maru@hotmail.com>
2019-10-02 22:23:06 -0500

    agregando cosas al portafolio

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="contacto">
     <h1>Mis datos de contacto</h1>
-    <section class="icon-list">
-  <!-- github -->
-  <i class="nes-icon github is-large"></i>
-
-  <!-- gmail -->
-  <i class="nes-icon gmail is-large"></i>
-
-  <!-- linkedin -->
-  <i class="nes-icon linkedin is-large"></i>
-
-  <!-- whatsapp -->
-  <i class="nes-icon whatsapp is-large"></i>
-</section>
+    <ul>
+      <li><i class="nes-icon github is-large"></i><a href="https://github.com/MarielaTorresB"> Mariela Torres</a></li>
+      <li><i class="nes-icon gmail is-large"></i>marielatorres@protonmail.com</li>
+      <li><i class="nes-icon linkedin is-large"></i><a href="www.linkedin.com/in/marielatorresb">Mariela Torres</a></li>
+      <li><i class="nes-smartphone"></i></li>
+    </ul>
   </div>
 </template>
