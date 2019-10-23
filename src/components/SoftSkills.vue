b60bc32d1549e5e881ed778dacb1de47b0655fb0
Mariela Torres <martz_maru@hotmail.com>
2019-10-23 18:44:29 -0500

    Guardar cambios

--- src/components/SoftSkills.vue
+++ src/components/SoftSkills.vue
@@ -2,48 +2,56 @@
   <div class="nes-container is-dark with-title is-centered">
     <p class="title">Soft Skills</p>
     <div class="badge-container">
-      <a href="#" class="nes-badge">
+      <li><a href="#" class="nes-badge">
         <span class="is-dark">Metodología Agile</span>
-      </a>
-      <a href="#" class="nes-badge">
+      </a></li>
+      <li><a href="#" class="nes-badge">
         <span class="is-primary">Autoaprendizaje</span>
-      </a>
-      <a href="#" class="nes-badge">
+      </a></li>
+      <li><a href="#" class="nes-badge">
         <span class="is-success">Trabajo en equipo</span>
-      </a>
-      <a href="#" class="nes-badge">
+      </a></li>
+      <li><a href="#" class="nes-badge">
         <span class="is-warning">Adaptabilidad</span>
-      </a>
-      <a href="#" class="nes-badge">
+      </a></li>
+      <li><a href="#" class="nes-badge">
         <span class="is-error">Solución de problemas</span>
-      </a>
+      </a></li>
     </div>
   </div>
 </template>
 
 <style scoped>
-span{
-    border: 5px dotted yellow;
-    display:block;
-    margin: 0 1rem;
+span {
+  border: 5px dotted yellow;
+  display: block;
+  margin: 0 1rem;
 }
 
- .nes-badge{
-    display: inline;
-} 
-
-.nes-badge span.is-primary:first-child, .nes-badge span.is-error:first-child, .nes-badge span.is-dark:first-child, .nes-badge span.is-success:first-child, .nes-badge span.is-warning:first-child {
-  width:initial;
+.nes-badge {
+  display: block;
 }
 
-.badge-container{
-    background-color: bisque;
-    display: flex;
-    flex-flow: row wrap;
+.nes-badge span.is-primary:first-child,
+.nes-badge span.is-error:first-child,
+.nes-badge span.is-dark:first-child,
+.nes-badge span.is-success:first-child,
+.nes-badge span.is-warning:first-child {
+  width: initial;
 }
 
-.nes-container{
-    padding: 0.5rem 0.1rem;
+.badge-container {
+  background-color: bisque;
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.nes-container {
+  padding: 0.5rem 0.1rem;
+}
+
+li {
+  float: left;
 }
 
 </style>
